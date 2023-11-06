79c26a6b19f70002e2d5311afab9fd8481bb3f06
akutsang <akuutsang@gmail.com>
2023-11-07 00:10:34 +0100

    downlod dependencies

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./app.scss";
+function App() {
+  return (
+    <>
+      <div>
+        <section>Hero</section>
+        <section>Parallax</section>
+        <section>Services</section>
+        <section>Portfolio</section>
+        <section>Portfolio2</section>
+        <section>Portfolio3</section>
+        <section>Contact</section>
+      </div>
+    </>
+  );
+}
+
+export default App;
