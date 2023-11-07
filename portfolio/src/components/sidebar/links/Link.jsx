07b654722636d21f23ff8ef87f5f7c43ae880638
akutsang <akuutsang@gmail.com>
2023-11-07 23:32:54 +0100

    href is a way of rerouting the page without react routes

--- portfolio/src/components/sidebar/links/Link.jsx
+++ portfolio/src/components/sidebar/links/Link.jsx
@@ -1,3 +1,20 @@
 export const Link = () => {
-  return <div></div>;
+  const items = [
+    "Homepage",
+    "Services",
+    "Contact",
+    "About",
+    "Resume",
+    "skills",
+  ];
+
+  return (
+    <div className="links">
+      {items.map((item) => {
+        <a href={`#${item}`} key={item}>
+          {item}
+        </a>;
+      })}
+    </div>
+  );
 };
