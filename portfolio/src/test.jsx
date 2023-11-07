520ec5f64224a2e63ce40e2da60b808fbabdfde9
akutsang <akuutsang@gmail.com>
2023-11-07 01:39:37 +0100

    add framer motion (while hover)

--- portfolio/src/test.jsx
+++ portfolio/src/test.jsx
@@ -6,8 +6,10 @@ export const Test = () => {
     <div className="test">
       <motion.div
         className="box"
-        animate={{ opacity: 0 }}
+        initial={{ opacity: 0.5, scale: 0.5 }}
+        // animate={{ opacity: 1, scale: 1, x: 500, y: 300 }}
         transition={{ duration: 2, delay: 2 }}
+        whileHover={{ opacity: 1, scale: 2 }}
       ></motion.div>
     </div>
   );
