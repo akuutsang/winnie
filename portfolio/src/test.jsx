54e86576b32c10d47d94a29474f5b57d7ca6475b
akutsang <akuutsang@gmail.com>
2023-11-07 01:57:22 +0100

    add button and event listerner

--- portfolio/src/test.jsx
+++ portfolio/src/test.jsx
@@ -1,16 +1,25 @@
 import "./app.scss";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 export const Test = () => {
+  const [open, setOpen] = useState(false);
+  const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
   return (
     <div className="test">
       <motion.div
         className="box"
-        initial={{ opacity: 0.5, scale: 0.5 }}
+        variants={variants}
+        // initial="hidden"
+        animate={open ? "visible" : "hidden"}
         transition={{ duration: 2 }}
         whileTap={{ opacity: 1, scale: 2 }}
         drag
       ></motion.div>
+      <button onClick={() => setOpen((prev) => !prev)}>click</button>
     </div>
   );
 };
