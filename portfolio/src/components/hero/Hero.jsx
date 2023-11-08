7f0162d82625c7bdaf3f4c8f76a0fbdd99e0fec0
akutsang <akuutsang@gmail.com>
2023-11-08 03:16:36 +0100

    add animation

--- portfolio/src/components/hero/Hero.jsx
+++ portfolio/src/components/hero/Hero.jsx
@@ -1,19 +1,50 @@
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="textContainer">
-          <h2>Hello there!</h2>
-          <h1>Welcome to my portfolio webpage</h1>
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>Hello there!</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Welcome to my portfolio webpage
+          </motion.h1>
           <div className="buttons">
-            <button>See the latest works</button>
-            <button>Contact me</button>
+            <motion.button variants={textVariants}>
+              See the latest works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact me</motion.button>
           </div>
-          <img className="img" src="/Wscroll.webp" alt="" />
-        </div>
+          <motion.img
+            className="img"
+            variants={textVariants}
+            src="/Wscroll.webp"
+            alt=""
+          />
+        </motion.div>
       </div>
+      <div className="slidingTextContainer">Akutsang</div>
       <div className="imageContainer">
         <img src="/robot-removebg-preview.png" alt="" />
       </div>
