3c5c011dbb574a8d02be01752d8ecf034dcbb487
akutsang <akuutsang@gmail.com>
2026-09-01 12:25:03 +0100

    update projects

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,31 +3,31 @@ module.exports = {
   env: { browser: true, es2020: true },
 
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
 
   ignorePatterns: [
-    'dist',
-    'node_modules',
-    'postcss.config.js',
-    'vite.config.js',
+    "dist",
+    "node_modules",
+    "postcss.config.js",
+    "vite.config.js",
   ],
 
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
 
   settings: {
-    react: { version: 'detect' }, // 👈 better than hardcoding 18.2
+    react: { version: "detect" }, // 👈 better than hardcoding 18.2
   },
 
-  plugins: ['react-refresh'],
+  plugins: ["react-refresh"],
 
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    "react-refresh/only-export-components": "off",
   },
 };
